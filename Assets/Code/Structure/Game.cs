e260a6fb1ff09340b09428e066996ef959faa417
Tony Lee <chunglee2018@u.northwestern.edu>
2017-10-29 21:31:21 -0500

    sprites updated

--- Assets/Code/Structure/Game.cs
+++ Assets/Code/Structure/Game.cs
@@ -11,7 +11,8 @@ namespace Assets.Code.Structure
         /// </summary>
         public static Game Ctx;
  
-        public static ScoreManager Score;
+        public static ScoreManager Score,Score2;
+
         public static Player Player;
         public static BulletManager Bullets;
         public static EdgeCollider2D Walls;
@@ -21,12 +22,13 @@ namespace Assets.Code.Structure
             Ctx = this;
 
             Score = GameObject.Find("ScoreText").GetComponent<ScoreManager>();
+            Score2 = GameObject.Find("ScoreText2").GetComponent<ScoreManager>();
             Player = GameObject.Find("Player").GetComponent<Player>();
             Walls = GameObject.Find("Walls").GetComponent<EdgeCollider2D>();
-            wallVertices.Add(new Vector2(100, 100));
-            wallVertices.Add(new Vector2(100, 300));
-            wallVertices.Add(new Vector2(300, 300));
-            wallVertices.Add(new Vector2(300, 100));
+            wallVertices.Add(new Vector2(0, 0));
+            wallVertices.Add(new Vector2(0, Screen.height));
+            wallVertices.Add(new Vector2(Screen.width, Screen.height));
+            wallVertices.Add(new Vector2(Screen.width, 0));
             Walls.points = wallVertices.ToArray();
 
             Bullets = new BulletManager(GameObject.Find("Bullets").transform);
