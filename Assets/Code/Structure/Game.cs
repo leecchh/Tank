3c92999471b4ba701d7981ad993813b0820f9b37
Tony Lee <chunglee2018@u.northwestern.edu>
2017-10-29 21:08:35 -0500

    walls

--- Assets/Code/Structure/Game.cs
+++ Assets/Code/Structure/Game.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace Assets.Code.Structure
 {
@@ -10,16 +11,24 @@ namespace Assets.Code.Structure
         /// </summary>
         public static Game Ctx;
  
-        // all of the things that we can about saving/loading
         public static ScoreManager Score;
         public static Player Player;
         public static BulletManager Bullets;
+        public static EdgeCollider2D Walls;
+        public List<Vector2> wallVertices = new List<Vector2>();
 
         internal void Start () {
             Ctx = this;
 
             Score = GameObject.Find("ScoreText").GetComponent<ScoreManager>();
             Player = GameObject.Find("Player").GetComponent<Player>();
+            Walls = GameObject.Find("Walls").GetComponent<EdgeCollider2D>();
+            wallVertices.Add(new Vector2(100, 100));
+            wallVertices.Add(new Vector2(100, 300));
+            wallVertices.Add(new Vector2(300, 300));
+            wallVertices.Add(new Vector2(300, 100));
+            Walls.points = wallVertices.ToArray();
+
             Bullets = new BulletManager(GameObject.Find("Bullets").transform);
         }
 
